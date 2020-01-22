bdc515412f58fd660ac91af13ba63d796d62821e
Keshari Nandan <keshari.nandan2@gmail.com>
2020-01-22 14:51:22 +0530

    [Fixesupdated request mapping interface namespace

--- src/Utilities/RequestMapper.php
+++ src/Utilities/RequestMapper.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TheNandan\TheRepository\Utilities;
+namespace TheNandan\TheRepository\Utilities\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
