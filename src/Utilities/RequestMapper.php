668d9fb866da8a33c588ce1784cb0d2cb2309730
Keshari Nandan <keshari.nandan2@gmail.com>
2020-01-22 16:17:39 +0530

    [Fixes] Completyed request mapper

--- src/Utilities/RequestMapper.php
+++ src/Utilities/RequestMapper.php
@@ -5,6 +5,7 @@ namespace TheNandan\TheRepository\Utilities;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use TheNandan\TheRepository\Utilities\Contracts\HasRequestMapping;
 
 /**
  * Class RequestMapper
@@ -23,7 +24,28 @@ class RequestMapper
      *
      * @throws \Exception
      */
-    public static function mapIntoArray(array $request, $model): array
+    public static function mapIntoArray(array $request, $model, $nullAllowed = false): array
+    {
+        $model = self::validateModelAndGetModel($model);
+        $keys = $model->mapRequest();
+        $data = [];
+        foreach ($keys as $k => $v) {
+            if (array_key_exists($v, $request) || $model->$k !== $request[$v]) {
+                $data[$k] = $request[$v];
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * This method validates the given model and returns the model object if valid
+     *
+     * @param $model
+     * @return Model
+     *
+     * @throws \Exception
+     */
+    private static function validateModelAndGetModel($model): Model
     {
         if (!($model instanceof Model)) {
             try {
@@ -33,6 +55,13 @@ class RequestMapper
             }
         }
 
+        if (!($model instanceof Model)) {
+            throw new \Exception('Given class ['.$model.'] is not a Laravel model.');
+        }
 
+        if (!method_exists($model, 'mapRequest')) {
+            throw new \Exception("Model [".get_class($model)."] must impliment the [HasRequestMapping] interface.");
+        }
+        return $model;
     }
 }
