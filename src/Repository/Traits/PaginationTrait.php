362ffcb6dd0d3c6f05c333739704c36895adf436
Keshari Nandan <keshari.nandan2@gmail.com>
2020-05-26 20:33:32 +0530

    [Fixes] Added search method

--- src/Repository/Traits/PaginationTrait.php
+++ src/Repository/Traits/PaginationTrait.php
@@ -24,13 +24,11 @@ trait PaginationTrait
         if (!empty($sortArray)) {
             $column = array_key_first($sortArray);
             $order = $sortArray[$column] ?? 'desc';
-            $model = $this->getModel();
-            if (method_exists($model, 'mapSortableFields')) {
-                $fields = $model->mapSortableFields();
-                $column = $fields[$column] ?? $column;
-            }
         }
         $this->sortBy($column, $order);
+        if ($search) {
+            $this->search($page->getSearchTerm());
+        }
         return $this->getQueryBuilder()->paginate($page->getPerPage(), $columns, 'page', $page->getPage());
     }
 
@@ -44,7 +42,41 @@ trait PaginationTrait
      */
     public function sortBy($column, $order = 'desc')
     {
+        $model = $this->getModel();
+        if (method_exists($model, 'mapSortableFields')) {
+            $fields = $model->mapSortableFields();
+            $column = $fields[$column] ?? $column;
+        }
         $this->setQueryBuilder($this->getQueryBuilder()->orderBy($column, $order));
+        return $this;
+    }
+
+    /**
+     * This method applies the search filter on the query
+     *
+     * @param $searchTerm
+     *
+     * @return $this
+     */
+    public function search($searchTerm)
+    {
+        if (null === $searchTerm) {
+            return $this;
+        }
+        $query = $this->getQueryBuilder();
+        $model = $this->getModel();
+        if (method_exists($model, 'setSearchableFields')) {
+            $fields = $model->setSearchableFields();
+            $counter = 0;
+            foreach ($fields as $field) {
+                if ($counter === 0) {
+                    $query = $query->where($field, 'like', "%$searchTerm%");
+                } else {
+                    $query = $query->orWhere($field, 'like', "%$searchTerm%");
+                }
+                $counter++;
+            }
+        }
         return $this;
     }
 }
